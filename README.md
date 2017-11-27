ddfdce4a257ba1d7142d81f4584fb83269774bdd
Shusaku Kaseda <skhr114@gmail.com>
2017-11-27 11:38:40 +0900

    add sampleProject

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # geant4  
-・Geant4の*windows*へのインストール  
+・Geant4の*windows*へのインストールマニュアル  
 ・geant4の学習  
 ・geant4プログラム  
 ・cernによるinstructionの日本語訳  
---
+++ sampleProject/detect_xray_emission/B1/include/B1Analysis.hh
@@ -0,0 +1,9 @@
+#ifndef B1Analysis_h
+#define B1Analysis_h 1
+// root形式で出力
+//
+// #include "g4root.hh"
+#include "g4csv.hh"
+// #include "g4xml.hh"
+
+#endif
